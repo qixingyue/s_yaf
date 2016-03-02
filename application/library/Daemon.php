f6e96e14bcc9c31445fc75976329ff6739760117
xingyue <qixingyue@126.com>
2016-03-02 11:02:42 +0800

    生成一个daemon进程

---
+++ application/library/Daemon.php
@@ -0,0 +1,52 @@
+<?php
+
+global $dup2_stdout,$dup2_stdin;
+
+class Daemon {
+
+	public static function changeToDaemon($stdoutfile = false,$stdinfile = false){
+			self::runAsDaemon(function(){},true,$stdoutfile,$stdinfile);	
+	}
+
+	public static function runAsDaemon($daemonTask,$postTaskGoOn = false,$stdoutfile = false,$stdinfile=false){
+		if(!is_callable($daemonTask)){
+			throw new Yaf_Exception("daemonTask must can be called ! ");
+		}
+		if(!extension_loaded("pcntl")){
+			throw new Yaf_Exception("pcntl must loaded ! ");
+		}
+		$pid = pcntl_fork();
+		if($pid == 0) {
+			//child process
+
+			self::dup2Stdout($stdoutfile);	
+			self::dup2Stdin($stdinfile);	
+
+			call_user_func($daemonTask);			
+			if(false === $postTaskGoOn){
+				exit();
+			}
+		} else if($pid > 0 ) {
+			exit();	
+		} else {
+			throw new Yaf_Exception("pcntl fork  error ! ");
+		}
+	}
+
+	public static function dup2Stdin($f){
+		if(false != $f){ 
+			fclose(STDIN);	
+			global $dup2_stdiin;
+			$dup2_stdiin = fopen($f,"w");
+		}
+	}
+
+	public static function dup2Stdout($f){
+		if(false != $f){ 
+			fclose(STDOUT);	
+			global $dup2_stdout ;
+			$dup2_stdout = fopen($f,"a+");
+		}
+	}
+
+}
